af0d7d06f4a6ee2cb33b1170b78b1308a0242a5a
Purushothaman9148 <46718711+Purushothaman9148@users.noreply.github.com>
2022-08-31 15:23:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+function MenuHomeClick() {
+    let menuhomeid = document.getElementById('home');
+    let menuaboutid = document.getElementById('about');
+    let menuskillsid = document.getElementById('skills');
+    let menuprojectid = document.getElementById('project');
+    let menucontactid = document.getElementById('contact');
+    menuhomeid.setAttribute("class", "nav-link active");
+    menuaboutid.setAttribute("class", "nav-link");
+    menuskillsid.setAttribute("class", "nav-link");
+    menuprojectid.setAttribute("class", "nav-link");
+    menucontactid.setAttribute("class", "nav-link");
+}
+
+function MenuAboutClick() {
+    let menuhomeid = document.getElementById('home');
+    let menuaboutid = document.getElementById('about');
+    let menuskillsid = document.getElementById('skills');
+    let menuprojectid = document.getElementById('project');
+    let menucontactid = document.getElementById('contact');
+    menuhomeid.setAttribute("class", "nav-link");
+    menuaboutid.setAttribute("class", "nav-link active");
+    menuskillsid.setAttribute("class", "nav-link");
+    menuprojectid.setAttribute("class", "nav-link");
+    menucontactid.setAttribute("class", "nav-link");
+}
+
+function MenuSkillsClick() {
+    let menuhomeid = document.getElementById('home');
+    let menuaboutid = document.getElementById('about');
+    let menuskillsid = document.getElementById('skills');
+    let menuprojectid = document.getElementById('project');
+    let menucontactid = document.getElementById('contact');
+    menuhomeid.setAttribute("class", "nav-link");
+    menuaboutid.setAttribute("class", "nav-link");
+    menuskillsid.setAttribute("class", "nav-link active");
+    menuprojectid.setAttribute("class", "nav-link");
+    menucontactid.setAttribute("class", "nav-link");
+}
+
+function MenuProjectsClick() {
+    let menuhomeid = document.getElementById('home');
+    let menuaboutid = document.getElementById('about');
+    let menuskillsid = document.getElementById('skills');
+    let menuprojectid = document.getElementById('project');
+    let menucontactid = document.getElementById('contact');
+    menuhomeid.setAttribute("class", "nav-link");
+    menuaboutid.setAttribute("class", "nav-link");
+    menuskillsid.setAttribute("class", "nav-link");
+    menuprojectid.setAttribute("class", "nav-link active");
+    menucontactid.setAttribute("class", "nav-link");
+}
+
+function MenuContactClick() {
+    let menuhomeid = document.getElementById('home');
+    let menuaboutid = document.getElementById('about');
+    let menuskillsid = document.getElementById('skills');
+    let menuprojectid = document.getElementById('project');
+    let menucontactid = document.getElementById('contact');
+    menuhomeid.setAttribute("class", "nav-link");
+    menuaboutid.setAttribute("class", "nav-link");
+    menuskillsid.setAttribute("class", "nav-link");
+    menuprojectid.setAttribute("class", "nav-link");
+    menucontactid.setAttribute("class", "nav-link active");
+}
